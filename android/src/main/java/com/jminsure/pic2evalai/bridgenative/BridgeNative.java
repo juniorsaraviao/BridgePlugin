37093ae79bfed8d1131d290217f27bd4beb96eca
Junior Saravia <junior.saravia.o@uni.pe>
2024-07-19 09:32:02 -0500

    Updates

--- android/src/main/java/com/jminsure/pic2evalai/bridgenative/BridgeNative.java
+++ android/src/main/java/com/jminsure/pic2evalai/bridgenative/BridgeNative.java
@@ -1,5 +1,7 @@
 package com.jminsure.pic2evalai.bridgenative;
 
+import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 public class BridgeNative {
@@ -7,5 +9,11 @@ public class BridgeNative {
     public String echo(String value) {
         Log.i("Echo", value);
         return value;
+    }
+
+    public void sendMessage(Context context, String message) {
+        Intent intent = new Intent("com.example.ACTION_SEND_MESSAGE");
+        intent.putExtra("message", message);
+        context.sendBroadcast(intent);
     }
 }
